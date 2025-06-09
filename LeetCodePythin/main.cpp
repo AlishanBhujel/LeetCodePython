737b1f6917b52d3f7b139524d61af958bc62c602
AlishanBhujel <alishan.191606@ncit.edu.np>
2025-06-09 07:47:35 +0545

    Initial commit for LeetCode Python in Xcode

---
+++ LeetCodePythin/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LeetCodePythin
+//
+//  Created by Alishan Bhujel on 6/9/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
